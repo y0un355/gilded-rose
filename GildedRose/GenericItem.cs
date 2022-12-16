0ff3feb3467f6268ccce84fa54c3f463ea274238
Youness <youness.fahmi.etu@univ-lille.fr>
2022-12-16 22:00:00 +0000

    Push

---
+++ GildedRose/GenericItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose
+{
+    public class GenericItem : Item
+    {
+        public GenericItem(string name, int sellIn, int quality) : base(name, sellIn, quality)
+        {
+        }
+
+        public override void Update()
+        {
+            this.sellIn--;
+            this.quality--;
+
+            if (this.sellIn < 0)
+                this.quality--;
+
+            if (this.quality < 0)
+                this.quality = 0;
+
+            if (this.quality >= 50)
+                this.quality = 50
+
+        }
+    }
+}
